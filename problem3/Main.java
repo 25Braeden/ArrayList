af2be2173fa7adfee6a1303404ec281f9fbf09a2
Braeden Audlin <125329807+25Braeden@users.noreply.github.com>
2024-04-29 06:28:00 -0700

    Update Main.java

--- problem3/Main.java
+++ problem3/Main.java
@@ -6,12 +6,14 @@ public class Main {
         stringList.add("Apple");
         stringList.add("Berry");
         stringList.add("Tomato");
+        stringList.add("evem");
         stringList.add("Dragonfruit");
         stringList.add("Watermelon");
         for (int i = 0; i < stringList.size(); i++) {
             String tempFruit = stringList.get(i);
             if(tempFruit.length() % 2 == 0) {
                 stringList.remove(tempFruit);
+                i--;
             }
         }
         System.out.println(stringList);
