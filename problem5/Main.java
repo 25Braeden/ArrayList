ba8f5d967f989fcd7c9350fa356fc2dcfad4058e
Braeden Audlin <125329807+25Braeden@users.noreply.github.com>
2024-05-02 10:35:44 -0700

    Update Main.java

--- problem5/Main.java
+++ problem5/Main.java
@@ -1,24 +1,46 @@
-package problem5;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static ArrayList<Integer> minToFront(ArrayList<Integer> list) {
-        if(list == null || list.isEmpty()) {
+        if (list == null || list.isEmpty()) {
             return null;
-        }
-        else {
-            Collections.sort(list);
+        } else {
+            int attempts = 0;
+            while (!isSorted(list)) {
+                Collections.shuffle(list);
+                attempts++;
+                System.out.println("Attempt: " + attempts);
+            }
             return list;
         }
     }
 
+    private static boolean isSorted(ArrayList<Integer> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) < list.get(i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        ArrayList<Integer> intList = new ArrayList<Integer>();
-        intList.add(11);
-        intList.add(1);
-        intList.add(141);
-        intList.add(21);
+        ArrayList<Integer> intList = new ArrayList<>();
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many numbers do you want?");
+        int amountOfNumbers = scanner.nextInt();
+
+        System.out.println("What is the upper bound?");
+        int upperBound = scanner.nextInt();
+
+        for (int i = 0; i < amountOfNumbers; i++) {
+            intList.add(random.nextInt(upperBound));
+        }
         System.out.println(intList);
         intList = minToFront(intList);
         System.out.println(intList);
